8e1fd7cbe84edc403f9de4801f76790aaef2161f
Lakshmihollat <148447524+Lakshmihollat@users.noreply.github.com>
2026-07-30 17:29:56 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> res(temperatures.size(), 0);
+        stack<pair<int, int>> stack; // pair: {temp, index}
+
+        for (int i = 0; i < temperatures.size(); i++) {
+            int t = temperatures[i];
+            while (!stack.empty() && t > stack.top().first) { //temp is greater than top temp in stack
+                auto pair = stack.top();   // top temp and intex in stack
+                stack.pop();
+                res[pair.second] = i - pair.second;  //res[index]= current index(high temp index) - index(of that temp for which we are looking for height temp than)
+            }
+            stack.push({t, i});  // current temp and index into stack.. cause we need to search for that also
+        }
+        return res;
+    }
+};
