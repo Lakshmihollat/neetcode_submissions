5fa97cbd86cbe84fc144e89b4c3b506431f86676
Lakshmihollat <148447524+Lakshmihollat@users.noreply.github.com>
2026-08-12 22:04:48 +0530

    Add: single-number - submission-3

---
+++ Data Structures & Algorithms/single-number/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        /*int res = 0;
+        for (int num : nums) {
+            res ^= num;  //every number xor with itself is 0
+        }
+        return res;*/
+        int res=0;
+        for(int num:nums)
+        {
+            res^=num;
+        }
+        return res;
+    }
+};
