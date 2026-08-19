ad2927621c11adc061cd81476213a7fec50c39b6
Lakshmihollat <148447524+Lakshmihollat@users.noreply.github.com>
2026-08-19 10:03:13 +0530

    Add: merge-sorted-array - submission-5

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        /*for(int i=0;i<n;i++){
+        if(find(nums1.begin(),nums1.end(),0)!=nums1.end())
+        {
+            auto f= find(nums1.begin(),nums1.end(),0);
+            nums1.erase(f);
+        }
+        }
+        for(int i=0;i<nums2.size();i++)
+        {
+            nums1.push_back(nums2[i]);
+        }
+        sort(nums1.begin(),nums1.end());*/
+        int j=0;
+        for(int i=m;i<nums1.size();i++)
+        {
+            nums1[i]=nums2[j];
+            j++;
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
