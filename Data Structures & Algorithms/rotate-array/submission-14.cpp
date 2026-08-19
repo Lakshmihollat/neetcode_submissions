ea3f10a5e57916b44b8c2a4a7381239a02cd8ac2
Lakshmihollat <148447524+Lakshmihollat@users.noreply.github.com>
2026-08-19 10:44:35 +0530

    Add: rotate-array - submission-14

---
+++ Data Structures & Algorithms/rotate-array/submission-14.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        /*reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+(k)%nums.size());
+        reverse(nums.begin()+k%nums.size(),nums.end());*/
+        int m=nums.size()-k;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+(k)%nums.size());
+        reverse(nums.begin()+(k)%nums.size(),nums.end());
+        
+        
+    }
+};
