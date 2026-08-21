35db1604e7c26a8f5028a584e5dd60c5519097fe
Lakshmihollat <148447524+Lakshmihollat@users.noreply.github.com>
2026-08-21 09:44:35 +0530

    Add: implement-stack-using-queues - submission-7

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-7.cpp
@@ -0,0 +1,23 @@
+class MyStack {
+public:
+queue<int> q;
+    MyStack() {}
+    void push(int x) {
+        q.push(x);
+        for (int i = q.size() - 1; i > 0; i--) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    int pop() {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+    int top() {
+        return q.front();
+    }
+    bool empty() {
+        return q.empty();
+    }
+};
