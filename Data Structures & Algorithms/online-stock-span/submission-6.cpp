d5895ef0c7f6d7f31317d1a77866e3c836165f2c
Lakshmihollat <148447524+Lakshmihollat@users.noreply.github.com>
2026-09-07 10:55:24 +0530

    Add: online-stock-span - submission-6

---
+++ Data Structures & Algorithms/online-stock-span/submission-6.cpp
@@ -0,0 +1,24 @@
+class StockSpanner {
+
+public:
+stack<pair<int, int>> stack; // pair: (price, span)
+    StockSpanner() {}
+
+    int next(int price) {
+       /* int span = 1;
+        while (!stack.empty() && stack.top().first <= price) {
+            span += stack.top().second;
+            stack.pop();
+        }
+        stack.push({price, span});
+        return span;*/
+        int span=1;
+        while(!stack.empty() && stack.top().first<=price)
+        {
+            span+=stack.top().second;
+            stack.pop();
+        }
+        stack.push({price,span});
+        return span;
+    }
+};
