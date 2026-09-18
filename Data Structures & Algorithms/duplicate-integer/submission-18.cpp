cb3cdf27b7915a79a83d7a622e77f0c2cc853415
Lakshmihollat <148447524+Lakshmihollat@users.noreply.github.com>
2026-09-18 12:08:29 +0530

    Add: duplicate-integer - submission-18

---
+++ Data Structures & Algorithms/duplicate-integer/submission-18.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int>n;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(n.count(nums[i]))
+            {
+                return true;
+            }
+            n.insert(nums[i]);
+        }
+        return false;
+    }
+};
